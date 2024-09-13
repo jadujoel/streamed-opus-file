0ccdff51cae376b0406ba84d84460a5733013137
Joel Löf <jlof@evolution.com>
2024-09-13 16:35:07 +0200

    fix: import opus from correct relative url

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 main()
 async function main() {
+  const decoderUrl = new URL('opus-stream-decoder.worker.js', import.meta.url).href;
+  const processorUrl = new URL('stream-audio-processor.js', import.meta.url).href;
+  const opusUrl = new URL('48kb.2ch.366384529314489.opus', import.meta.url).href;
+
   const context = new AudioContext();
 
-  const decoder = new Worker(new URL('opus-stream-decoder.worker.js', import.meta.url).href, { type: 'module' })
-  await context.audioWorklet.addModule(new URL('stream-audio-processor.js', import.meta.url).href);
+  const decoder = new Worker(decoderUrl, { type: 'module' });
+  await context.audioWorklet.addModule(processorUrl);
 
   const node = new AudioWorkletNode(context, 'stream-audio-processor', { numberOfInputs: 0, numberOfOutputs: 1, outputChannelCount: [2] });
   decoder.addEventListener('message', (ev: MessageEvent<{ left: Float32Array, right: Float32Array, index: number }>) => {
@@ -11,9 +15,8 @@ async function main() {
   });
 
   node.connect(context.destination);
-
   decoder.postMessage(JSON.stringify(({
     type: 'start',
-    url: '/48kb.2ch.366384529314489.opus'
+    url: opusUrl
   })))
 }
